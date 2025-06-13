c62e40dcaaac09a67ff0cf73f546ddff4d47a6c1
kerimtetik <abdulkerimtetik0@gmail.com>
2025-06-14 00:51:31 +0300

    v.1.0.8

--- src/components/HeroLanding.js
+++ src/components/HeroLanding.js
@@ -5,7 +5,7 @@ const HeroLanding = () => {
     <section className="min-h-screen flex items-center justify-between bg-gradient-to-br from-purple-800 via-purple-900 to-black text-white px-6 lg:px-24 py-20">
       {/* Sol Metin Alanı */}
       <div className="flex-1 space-y-6">
-        <p className="uppercase tracking-wider text-gray-300 text-sm">Yazılımın ustası</p>
+        <p className="uppercase tracking-wider text-gray-300 text-sm">Yazılım Geliştirici</p>
         <h1 className="text-5xl md:text-6xl font-extrabold leading-tight">
           Kerim Tetik <br />
           <span className="text-purple-300">Backend Developer</span>
@@ -22,7 +22,7 @@ const HeroLanding = () => {
       <div className="flex-1 hidden lg:flex justify-center items-center">
         <img
           src="/profil.jpg" // 📂 public klasörüne 'profil.jpg' dosyasını yerleştirmeyi unutma
-            alt="profil"
+          alt="profil"
           className="rounded-lg shadow-2xl w-[300px] h-[400px] object-cover transform hover:scale-105 transition duration-500"
         />
       </div>
